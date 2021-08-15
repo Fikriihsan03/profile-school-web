01239e89e5337cc10fc92d70253baa5610c06bf5
Fikri ihsan <mfikriihsan03@gmail.com>
2021-08-15 20:54:47 +0700

    routing with navbar

--- components/Navbar/navbar.js
+++ components/Navbar/navbar.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import Image from "next/image";
+import Link from "next/link";
 import schoolLogo from "../../assets/logo-sekolah.png";
 
 const Navbar = ({}) => {
@@ -32,24 +33,21 @@ const Navbar = ({}) => {
         } w-full block  lg:flex lg:justify-end lg:items-center lg:w-auto`}
       >
         <div className="text-sm lg:flex-grow">
-          <a
-            href="#responsive-header"
-            className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4"
-          >
-            Docs
-          </a>
-          <a
-            href="#responsive-header"
-            className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4"
-          >
-            Examples
-          </a>
-          <a
-            href="#responsive-header"
-            className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white"
-          >
-            Blog
-          </a>
+          <Link href="/">
+            <a className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
+              Home
+            </a>
+          </Link>
+          <Link href="/about">
+            <a className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
+              About
+            </a>
+          </Link>
+          <Link href="/contact">
+            <a className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
+              Contact
+            </a>
+          </Link>
         </div>
         <div>
           <a

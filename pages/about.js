be164d9d08c59e7e0094e25b2e66de4ffc36e9ee
Fikri ihsan <mfikriihsan03@gmail.com>
2021-08-15 09:11:35 +0700

    routing complete

---
+++ pages/about.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "../components/Navbar/navbar";
+
+const About = () => {
+  return (
+    <div>
+      <Navbar />
+      <h1>Welcome to about page</h1>;
+    </div>
+  );
+};
+
+export default About;
---
+++ pages/api/contact.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "../components/Navbar/navbar";
+
+const Contact = () => {
+  return (
+    <div>
+      <Navbar />
+      <h1>Welcome to contact page</h1>;
+    </div>
+  );
+};
+
+export default Contact;

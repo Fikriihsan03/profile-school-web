78466297fb286bd4219d547abf77ef0af2ca1e81
Fikri ihsan <mfikriihsan03@gmail.com>
2022-06-14 22:55:47 +0700

    store to cloud

--- pages/contact.js
+++ pages/contact.js
@@ -1,8 +1,40 @@
-import React from "react";
+import { useState } from "react";
 import Layout from "../components/Layout/layout";
 import Navbar from "../components/Navbar/navbar";
 
 const Contact = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+  const [submitted, setSubmitted] = useState(false);
+  console.log(message);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Sending");
+    let data = {
+      name,
+      email,
+      message,
+    };
+    fetch("/api/contact", {
+      method: "POST",
+      headers: {
+        Accept: "application/json, text/plain, */*",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    }).then((res) => {
+      console.log("Response received");
+      if (res.status === 200) {
+        console.log("Response succeeded!");
+        setSubmitted(true);
+        setName("");
+        setEmail("");
+        setBody("");
+      }
+    });
+  };
   return (
     <Layout title="Contact">
       <Navbar textColor="black" />
@@ -17,20 +49,33 @@ const Contact = () => {
             <input
               type="text"
               placeholder="Nama Anda"
+              onChange={(e) => {
+                setName(e.target.value);
+              }}
               className="border-2 border-grey-900 rounded-md md:shadow-md w-3/5 md:w-1/4 md:h-10 p-2 md:p-3 md:mr-80 md:ml-4 "
             />
             <input
               type="Email"
               placeholder="Email Anda"
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}
               className="border-2 border-grey-900 rounded-md md:shadow-md w-3/5 md:w-1/4 md:h-10 p-2 md:p-3 md:mr-80 md:ml-4"
             />
             <textarea
               placeholder="your message"
-              className="w-11/12 h-48 border-2 border-grey-900 p-2 rounded md:w-1/2 md:h-48  "
+              onChange={(e) => {
+                setMessage(e.target.value);
+              }}
+              style={{ height: "300px" }}
+              className="w-11/12 h-56 border-2 border-grey-900 p-2 rounded md:w-1/2 md:h-72  "
             />
             <input
               type="submit"
               value="submit"
+              onClick={(e) => {
+                handleSubmit(e);
+              }}
               className="transition duration-500 ease-in-out bg-green-600  transform hover:-translate-y-1 hover:scale-110 text-white text-center border-2 border-grey-900 rounded-md  w-1/4 p-2 md:w-24 md:h-10 "
             />
           </div>
